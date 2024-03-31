634e544c8ec8a90baa8cf0969e5c05f80d480b7a
DgTanDat <21521927@gm.uit.edu.vn>
2024-03-31 21:59:26 +0700

    upload code

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/CE232_IDF/i2c_selftest/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/HP/esp/v5.2.1/esp-idf/.git")
+set(GIT_DIR "D:/CE232_IDF/i2c_selftest/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
